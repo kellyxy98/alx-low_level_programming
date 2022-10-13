ff4de38488026f42d9216c2c98078842fc277f64
kellyxy98 <kelechiabara98@gmail.com>
2022-10-13 12:45:44 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints in the put function
+ *
+ * Return: Always 0 (sucessful)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
