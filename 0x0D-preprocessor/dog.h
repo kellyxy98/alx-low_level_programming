30bc517cd6e2daffa4f6b8a4b9cf081b51b5c392
kellyxy98 <kelechiabara98@gmail.com>
2022-11-14 04:22:23 -0800

    task 0

---
+++ 0x0D-preprocessor/dog.h
@@ -0,0 +1,30 @@
+#ifndef DOG_H_
+#define DOG_H_
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+/**
+ * struct dog - struct that stores imformation about a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} ;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
+#endif
