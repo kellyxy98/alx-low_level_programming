9617f1cc179e9166c4033db257c0aa2c234db98f
kellyxy98 <kelechiabara98@gmail.com>
2022-11-24 07:37:24 -0800

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,24 +1,19 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
- * listint_len - Returns the number of elements
- * in a linked listint_t list
- * @h: A pointer to the head of the listint_t list
- *
- *  Return: The number of elements in the listint_t list
+ * list_len - returns the number of elements in a list
+ * @h: singly linked list
+ * Return: number of elements in a list
  */
-
-
-size_t listint_len(const listint_t *h)
+size_t list_len(const list_t *h)
 {
-	size_t nodes = 0;
+	size_t elem;
 
-	while (h)
+	elem = 0;
+	while (h != NULL)
 	{
-		nodes++;
 		h = h->next;
+		elem++;
 	}
-
-	return (nodes);
+	return (elem);
 }
